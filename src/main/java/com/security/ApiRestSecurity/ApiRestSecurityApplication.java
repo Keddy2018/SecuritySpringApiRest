5e79ebd34b029054735062030cde051d71937c6f
keddy <recostawalter@gmail.com>
2022-06-01 19:15:07 -0300

    fix: carga inicial

---
+++ src/main/java/com/security/ApiRestSecurity/ApiRestSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.security.ApiRestSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/ApiRestSecurity/ApiRestSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.ApiRestSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
